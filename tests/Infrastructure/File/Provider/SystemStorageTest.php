365c378a215b2c9188b617250771abed0531cea5
Rafal Salamon <rasa@salamonrafal.pl>
2018-08-13 08:49:02 +0200

    Add empty unit test for Client: Storage

--- tests/Infrastructure/File/Provider/SystemStorageTest.php
+++ tests/Infrastructure/File/Provider/SystemStorageTest.php
@@ -1,2 +1,17 @@
 <?php
 namespace PDT\Tests\Infrastructure\File\Provider;
+
+use PDT\Tests\Infrastructure\File\TestDataProvider;
+
+class SystemStorageTest extends TestDataProvider
+{
+    public function test_move_uploaded_file(): void
+    {
+
+    }
+
+    public function test_get_file_information(): void
+    {
+
+    }
+}
